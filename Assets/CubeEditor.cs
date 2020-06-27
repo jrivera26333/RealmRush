b9be194e46f1a3235b55423a3d231a843e35a3d1
jrivera26333 <jrivera26333@gmail.com>
2020-06-27 11:33:10 -0500

    Added a snapping grid function to the cubes via script

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode][SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [Range(1f, 20f)]
+    [SerializeField] float gridSize = 0;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; //We get a decimal and then round to nearest int to snap
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize; //We get a decimal and then round to nearest int to snap
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x /gridSize + "," + snapPos.z /gridSize; //Since we have a rounded value we are getting the grid cord
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
