6e744af3a1c2a5b4fc1fdd943c080e143a5d82e6
jrivera26333 <jrivera26333@gmail.com>
2020-06-29 19:20:36 -0500

    Added basic enemy pathing by making a List of blocks then move through the list the a coroutine and transform.position

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -20,10 +20,13 @@ public class CubeEditor : MonoBehaviour
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; //We get a decimal and then round to nearest int to snap
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize; //We get a decimal and then round to nearest int to snap
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
 
         textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = snapPos.x /gridSize + "," + snapPos.z /gridSize; //Since we have a rounded value we are getting the grid cord
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize; //Since we have a rounded value we are getting the grid cord
+        textMesh.text = labelText;
+        gameObject.name = labelText;
 
-        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
     }
 }
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        Debug.Log("Starting the patrol!");
+        foreach (Waypoint wayPoint in path)
+        {
+            Debug.Log("Visiting block: " + wayPoint.name);
+            transform.position = wayPoint.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+
+        Debug.Log("Ending Patrol!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
